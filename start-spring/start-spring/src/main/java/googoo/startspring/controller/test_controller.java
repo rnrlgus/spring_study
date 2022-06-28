91a596f6902a3c60e504a349889267490a2c1685
rnrlgus <rnrl4611@gmail.com>
2022-06-28 17:39:11 +0900

    static, mvc, api

--- start-spring/start-spring/src/main/java/googoo/startspring/controller/test_controller.java
+++ start-spring/start-spring/src/main/java/googoo/startspring/controller/test_controller.java
@@ -3,6 +3,8 @@ package googoo.startspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class test_controller {
@@ -14,4 +16,36 @@ public class test_controller {
         return "hello";
     }
 
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http 프로토콜의 body 부분에 직접 데이터를 넣어주겠다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // hello koo
+    }
+
+    static class Hello {
+        public String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
 }
