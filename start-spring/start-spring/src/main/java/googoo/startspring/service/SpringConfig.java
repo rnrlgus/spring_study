4ce2292bef4e99c48e3f23318266c63a5147e0ed
rnrlgus <rnrl4611@gmail.com>
2022-07-09 18:17:57 +0900

    change db system ( memory to h2 )

--- start-spring/start-spring/src/main/java/googoo/startspring/service/SpringConfig.java
+++ start-spring/start-spring/src/main/java/googoo/startspring/service/SpringConfig.java
@@ -1,13 +1,25 @@
 package googoo.startspring.service;
 
+import googoo.startspring.repository.JdbcMemberRepository;
 import googoo.startspring.repository.MemberRepository;
 import googoo.startspring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // 직접 스프링 빈 등록하기
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
