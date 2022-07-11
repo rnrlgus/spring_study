5fcb509ce00b1b3210f25d2a42a0a3bb07091b30
rnrlgus <rnrl4611@gmail.com>
2022-07-11 17:11:42 +0900

    Spring Data JPA

--- start-spring/start-spring/src/main/java/googoo/startspring/service/SpringConfig.java
+++ start-spring/start-spring/src/main/java/googoo/startspring/service/SpringConfig.java
@@ -13,30 +13,26 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-//    private DataSource dataSource;
-//
-//    @Autowired
-//    public SpringConfig(DataSource dataSource) {
-//        this.dataSource = dataSource;
-//    }
 
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        //return new MemoryMemberRepository();
-        //return new JdbcMemberRepository(dataSource);
-        //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
+
 }
