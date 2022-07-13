744f667012a8bffbb9ed4fe91a1ae820748b405e
rnrlgus <rnrl4611@gmail.com>
2022-07-13 20:57:26 +0900

    member domain

---
+++ core/core/src/main/java/googoo/core/member/Grade.java
@@ -0,0 +1,6 @@
+package googoo.core.member;
+
+public enum Grade {
+    BASIC,
+    VIP
+}
---
+++ core/core/src/main/java/googoo/core/member/MemberRepository.java
@@ -0,0 +1,8 @@
+package googoo.core.member;
+
+public interface MemberRepository {
+
+    void save(Member member);
+
+    Member findById(Long memberId);
+}
