9ef7a8d6bea085bf1dd33e7d616ab7fb03d199d5
rnrlgus <rnrl4611@gmail.com>
2022-07-28 17:12:01 +0900

    using lombok

--- core/core/src/main/java/googoo/core/order/OrderServiceImpl.java
+++ core/core/src/main/java/googoo/core/order/OrderServiceImpl.java
@@ -6,22 +6,22 @@ import googoo.core.discount.RateDiscountPolicy;
 import googoo.core.member.Member;
 import googoo.core.member.MemberRepository;
 import googoo.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // using lombok (constructor)
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    private DiscountPolicy discountPolicy;
+    private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
