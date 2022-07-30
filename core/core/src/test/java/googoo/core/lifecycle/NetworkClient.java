ec127a1a7785fdba533329d9fc9ef74918279d22
rnrlgus <rnrl4611@gmail.com>
2022-07-30 21:21:00 +0900

    using initMethod, destroyMethod

--- core/core/src/test/java/googoo/core/lifecycle/NetworkClient.java
+++ core/core/src/test/java/googoo/core/lifecycle/NetworkClient.java
@@ -1,44 +1,34 @@
 package googoo.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
-
+public class NetworkClient {
     private String url;
-
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
     }
-
     public void setUrl(String url) {
         this.url = url;
     }
-    
-    // 서비스 시작할 때 호출하는 메서드
+    //서비스 시작시 호출
     public void connect() {
-        System.out.println("connect = " + url);
+        System.out.println("connect: " + url);
     }
-
     public void call(String message) {
-        System.out.println("call = " + url + " message = " + message);
+        System.out.println("call: " + url + " message = " + message);
     }
-
-    // 서비스 종료시 호출
-    public void disconnect() {
-        System.out.println("close " + url);
+    //서비스 종료시 호출
+    public void disConnect() {
+        System.out.println("close + " + url);
     }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
-
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
-        disconnect();
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disConnect();
     }
 }
