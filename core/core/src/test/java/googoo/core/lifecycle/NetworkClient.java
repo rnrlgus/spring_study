a36456ca3df174d5fe733dce86bfc85dfa09da25
rnrlgus <rnrl4611@gmail.com>
2022-07-30 21:36:43 +0900

    using annotation @PostConstruct, @PreDestroy when using library, use previous method

--- core/core/src/test/java/googoo/core/lifecycle/NetworkClient.java
+++ core/core/src/test/java/googoo/core/lifecycle/NetworkClient.java
@@ -22,11 +22,15 @@ public class NetworkClient {
     public void disConnect() {
         System.out.println("close + " + url);
     }
+
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
+
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disConnect();
