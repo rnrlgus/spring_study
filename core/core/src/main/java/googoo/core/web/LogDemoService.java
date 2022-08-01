d8e71abee6ad5bc7b2fa61b7b62a361ed2648b64
rnrlgus <rnrl4611@gmail.com>
2022-08-01 16:46:24 +0900

    request scope and provider

---
+++ core/core/src/main/java/googoo/core/web/LogDemoService.java
@@ -0,0 +1,17 @@
+package googoo.core.web;
+
+import googoo.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+    public void logic(String id) {
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
+        myLogger.log("service id: " + id);
+    }
+}
