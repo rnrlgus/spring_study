85465099083756a52d1d56448094926511a4c49b
rnrlgus <rnrl4611@gmail.com>
2022-08-28 18:18:01 +0900

    member repository

--- spring-JPA/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ spring-JPA/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -35,7 +35,7 @@ public class Category {
     // 연관관계 메서드
     public void addChildCategory(Category child) {
         this.child.add(child);
-        child.setChild(this);
+        child.setParent(this);
     }
 
 }
